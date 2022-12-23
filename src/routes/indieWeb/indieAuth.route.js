b914463ffb37131f02a8b29cf3b3d086462f93e6
Brandon Trebitowsk <brandon@swoopnow.com>
2022-12-22 21:03:31 -0700

    added themes folder to git

--- src/routes/indieWeb/indieAuth.route.js
+++ src/routes/indieWeb/indieAuth.route.js
@@ -5,6 +5,7 @@ const auth = require('../../middlewares/auth.middleware');
 const router = express.Router();
 
 router.get('/authorize', auth(true), indieAuthController.authorize);
+router.post('/authorize', auth(true), indieAuthController.token);
 router.get('/approve', auth(true), indieAuthController.approve);
 router.post('/token', auth(false), indieAuthController.token);
 router.get('/token', indieAuthController.verifyToken);
